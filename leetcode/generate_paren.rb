597d345d3d38e94b780ffd1673d76b9cc5cb1f45
Sayak Chatterjee <Sayak.Chatterjee@tufts.edu>
2022-09-18 04:01:56 -0400

    Update generate_paren.rb

--- leetcode/generate_paren.rb
+++ leetcode/generate_paren.rb
@@ -1,6 +1,8 @@
 # Generate Parenthesis
 # Leetcode problem 22
 
+# Solution 1 - Global variables - less time consuming but ugly
+
 $stack
 $result
 
@@ -27,4 +29,27 @@ def backtrack(open, close, n)
         backtrack(open, close + 1, n)
         $stack.pop
     end
+end
+
+
+# Solution 2 - all params - more time consuming but cleaner
+
+# @param {Integer} n
+# @return {String[]}
+def generate_parenthesis(n)
+    backtrack(0, 0, n, "", [])
+end
+
+def backtrack(open, close, n, stack, result)
+    if (open < n)
+        backtrack(open + 1, close, n, stack + "(", result)
+    end
+    
+    if (close < open)
+        backtrack(open, close + 1, n, stack + ")", result)
+    end
+    
+    result << stack if (open == close and open == n)
+    
+    result
 end
